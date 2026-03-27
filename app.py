2860d3650f84519da4678690d09b0de5b6580d08
... <...>
2026-03-27 14:51:09 -0400

    changes

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import requests
 from io import BytesIO
 import json
 
+import os
+
 app = Flask(__name__)
 CORS(app)  # allows frontend to call this backend
 
@@ -129,4 +131,5 @@ def predict_from_upload():
 # ─── Run ─────────────────────────────────────────────────────────────────────
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000,debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
